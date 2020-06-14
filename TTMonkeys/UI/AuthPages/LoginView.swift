29d5d969dd13c840b7e2fad8750c8abaf57e165b
Liam Smith <liam.smith@appsinthesky.com>
2020-06-14 18:47:07 +0100

    - built the wire frame for the login page

--- TTMonkeys/UI/AuthPages/LoginView.swift
+++ TTMonkeys/UI/AuthPages/LoginView.swift
@@ -13,20 +13,34 @@ struct LoginView: View {
     @State private var email: String = ""
     @State private var password: String = ""
     
+    @State private var isNavigationBarHidden = true
+    
     @State private var result: String = ""
     
     var body: some View {
-        VStack {
-            TextField("Email", text: $email)
-            SecureField("Password", text: $password)
-            
-            Button(action: {
-                self.doLogin()
-            }) {
-                Text("Login")
+        NavigationView {
+            VStack {
+                TextField("Email", text: $email)
+                SecureField("Password", text: $password)
+                
+                Button(action: {
+                    self.doLogin()
+                }) {
+                    Text("Login")
+                }
+                
+                NavigationLink(destination: SignupView()) {
+                    Text("Sign Up")
+                }
+                
+                Button(action: {
+                    self.didForgetPassword()
+                }) {
+                    Text("Forgot Password")
+                }
+                
+                Text(result)
             }
-            
-            Text(result)
         }
     }
     
@@ -48,6 +62,21 @@ struct LoginView: View {
         }
         else {
             result = RegistrationError.invalidEmail.localizedDescription
+        }
+    }
+    
+    private func didForgetPassword() {
+        guard email.isValidEmail() else {
+            result = "Please enter your email address"
+            return
+        }
+        
+        UserModel.instance.forgotPassword(email: email) { error in
+            if let error = error {
+                self.result = error.localizedDescription
+                return
+            }
+            self.result = "Sent a password reset email"
         }
     }
 }
